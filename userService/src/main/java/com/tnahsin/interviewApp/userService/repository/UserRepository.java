8254f3c5bf36f2976167aa545f891c7fac0ad26f
T-nahsin <nishant16405@gmail.com>
2025-10-10 01:32:55 +0530

    Changed some features

--- userService/src/main/java/com/tnahsin/interviewApp/userService/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tnahsin.interviewApp.userService.repository;
-
-import com.tnahsin.interviewApp.userService.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-
-}
