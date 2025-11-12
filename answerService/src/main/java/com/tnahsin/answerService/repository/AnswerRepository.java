fa715698084850fae3f953ada619f2d6552abaf0
T-nahsin <nishant16405@gmail.com>
2025-11-12 18:30:15 +0530

    Delete answerService directory

--- answerService/src/main/java/com/tnahsin/answerService/repository/AnswerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tnahsin.answerService.repository;
-
-import com.tnahsin.answerService.model.Answer;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AnswerRepository extends MongoRepository<Answer, String> {
-
-}
