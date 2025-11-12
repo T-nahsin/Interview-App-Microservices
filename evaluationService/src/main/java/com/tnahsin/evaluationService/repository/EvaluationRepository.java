cfede24cbee1d599fcc11d246ed4a71df2632733
T-nahsin <nishant16405@gmail.com>
2025-11-12 18:31:31 +0530

    Delete evaluationService directory

--- evaluationService/src/main/java/com/tnahsin/evaluationService/repository/EvaluationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tnahsin.evaluationService.repository;
-
-import com.tnahsin.evaluationService.model.Evaluations;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface EvaluationRepository extends MongoRepository<Evaluations,String> {
-
-    Evaluations findByQuesId(String quesId);
-}
