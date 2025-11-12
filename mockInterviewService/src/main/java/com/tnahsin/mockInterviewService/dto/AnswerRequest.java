fd489f74a9b1984279fed7ee38be1ecce795816b
T-nahsin <nishant16405@gmail.com>
2025-11-12 18:31:44 +0530

    Delete mockInterviewService directory

--- mockInterviewService/src/main/java/com/tnahsin/mockInterviewService/dto/AnswerRequest.java
+++
@@ -1,9 +0,0 @@
-package com.tnahsin.mockInterviewService.dto;
-
-
-import lombok.Data;
-
-@Data
-public class AnswerRequest {
-    private String answer;
-}
--- mockInterviewService/src/main/java/com/tnahsin/mockInterviewService/repository/MockInterviewRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tnahsin.mockInterviewService.repository;
-
-import com.tnahsin.mockInterviewService.model.MockInterview;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface MockInterviewRepository extends MongoRepository<MockInterview,String> {
-}
